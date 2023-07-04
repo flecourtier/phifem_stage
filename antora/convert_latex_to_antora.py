8407aac25f84785b271613ffa972e6d87de6be0f
flecourtier <lecourtier.frederique@gmail.com>
2023-07-04 16:50:05 +0200

    Convert Latex to Antora

---
+++ antora/convert_latex_to_antora.py
@@ -0,0 +1,303 @@
+import numpy as np
+import os
+import sys
+import shutil
+import re
+
+"""
+But : Convertir un fichier latex en une documentation antora complète
+"""
+
+root_dir = "/home/lecourtier/Bureau/rapport/"
+
+source_dir = "rapport/"
+rapport_file = root_dir + source_dir + "rapport.tex"
+result_dir = root_dir + "antora/modules/ROOT/"
+page_dir = result_dir + "pages/"
+images_dir = result_dir + "assets/images/"
+
+def search_word_in_line(word, line):
+    # return true if word is in line
+    return word in line
+
+# list of all the sections and subsections
+def list_sections():
+    file_read = open(rapport_file, 'r')
+    # we start by complete sections
+    section_files = []
+    empty_sections = []
+    while line := file_read.readline():
+        if search_word_in_line("\input", line):
+            section_files.append(line.split("{")[1].split("}")[0])
+        if search_word_in_line("\section", line):
+            section_files.append("")
+            empty_sections.append(line.split("{")[1].split("}")[0])
+    section_files.pop(0)
+    
+    sections = {}
+    for section in section_files:
+        if section!="":
+            file_read = open(root_dir + source_dir + section + ".tex", 'r')
+            subsections = []
+            while line := file_read.readline():
+                if search_word_in_line("\section", line):
+                    key = line.split("{")[1].split("}")[0]
+                if search_word_in_line("\subsection", line):
+                    subsections.append(line.split("{")[1].split("}")[0])
+            sections[key] = subsections
+        else:
+            key = empty_sections.pop(0)
+            sections[key] = None
+
+    return section_files, sections
+
+def create_nav_file(section_files,sections):
+    # create the nav.adoc file
+    nav_file = result_dir + "nav.adoc"
+    file_write = open(nav_file, 'w')
+
+    file_write.write("* xref:main_page.adoc[PhiFEM project]\n")
+
+    for i,(section,subsections) in enumerate(sections.items()):        
+        if section_files[i]!="":
+            section_file_name = section_files[i].split("/")[1]
+            file_write.write("** xref:" + section_file_name + ".adoc[" + section + "]\n")
+            if subsections!=None:
+                for j,subsection in enumerate(subsections):
+                    file_write.write("*** xref:" + section_file_name + "/subsec_" + str(j) + ".adoc[" + subsection + "]\n")
+        else:
+            section_file_name = "section_" + str(i)
+            file_write.write("** xref:" + section_file_name + ".adoc[" + section + "]\n")
+
+    file_write.close()
+
+def create_nav(section_files,sections):
+    # remove all the files in the page directory
+    if os.path.exists(page_dir):
+        shutil.rmtree(page_dir)
+    os.mkdir(page_dir)
+
+    for i,(section,subsections) in enumerate(sections.items()):        
+        if section_files[i]!="":
+            section_file_name = section_files[i].split("/")[1]
+            section_file = open(page_dir + section_file_name + ".adoc", 'w')
+            section_file.write("= " + section + "\n")
+            if subsections!=None:
+                os.mkdir(page_dir + section_file_name)
+                for j,subsection in enumerate(subsections):
+                    subsection_file = open(page_dir + section_file_name + "/subsec_" + str(j) + ".adoc", 'w')
+                    subsection_file.write("= " + subsection + "\n")
+                    subsection_file.close()
+            section_file.close()
+        else:
+            section_file_name = "section_" + str(i)
+            section_file = open(page_dir + section_file_name + ".adoc", 'w')
+            section_file.write("= " + section + "\n")
+            section_file.close()     
+
+def create_main_page_file(section_files,sections):
+    # create the nav.adoc file
+    main_page_file = page_dir + "main_page.adoc"
+    file_write = open(main_page_file, 'w')
+
+    file_write.write("# Phi-FEM Project\n\n")
+    file_write.write("The features include\n\n")
+
+    for i,(section,subsections) in enumerate(sections.items()):        
+        if section_files[i]!="":
+            section_file_name = section_files[i].split("/")[1]
+            file_write.write("** xref:" + section_file_name + ".adoc[" + section + "]\n")
+        else:
+            section_file_name = "section_" + str(i)
+            file_write.write("** xref:" + section_file_name + ".adoc[" + section + "]\n")
+
+    file_write.close()   
+
+# copy all the images of the tex report in 
+def cp_images():       
+    if os.path.exists(images_dir):
+        shutil.rmtree(images_dir)
+    shutil.copytree(root_dir + source_dir + "images/", images_dir)
+
+def test_fig(line):
+    possible_ref = ["Figure \\ref","Figure~\\ref","Fig \\ref","Fig~\\ref","Fig.~\\ref","Fig.\\ref"]
+    for ref in possible_ref:
+        if search_word_in_line(ref,line):
+            return ref,True
+    return None,False
+
+def test_section(line):
+    possible_ref = ["Section \\ref","Section~\\ref","Sec \\ref","Sec~\\ref","Sec.~\\ref","Sec.\\ref"]
+    for ref in possible_ref:
+        if search_word_in_line(ref,line):
+            return ref,True
+    return None,False
+
+def get_label_sections(section_files):
+    label_sections = {}
+    for section_file in section_files:
+        if section_file!="":
+            section_file_name = section_file.split("/")[1]
+            file_read = open(root_dir + source_dir + section_file + ".tex", 'r')
+            
+            num_subsection = -1
+            while line := file_read.readline():
+                if search_word_in_line("\section", line):
+                    if search_word_in_line("\label", line):
+                        label_sections[line.split("\label{")[1].split("}")[0]]=section_file_name
+                
+                if search_word_in_line("\subsection", line):
+                    num_subsection += 1
+                    subsection_name = line.split("{")[1].split("}")[0]
+                    if search_word_in_line("\label", line):
+                        label_sections[line.split("\label{")[1].split("}")[0]]=section_file_name+"/subsec_"+str(num_subsection)
+
+                if search_word_in_line("\subsubsection", line):
+                    if search_word_in_line("\label", line):
+                        label_sections[line.split("\label{")[1].split("}")[0]]=section_file_name+"/subsec_"+str(num_subsection)
+
+    return label_sections
+
+def cp_section(section_file,label_sections):
+    file_read = open(root_dir + source_dir + section_file + ".tex", 'r')
+    
+    name_section_file = section_file.split("/")[1]
+    file_write = open(page_dir + name_section_file + ".adoc", 'w')
+    
+    num_subsection = -1
+    subsection_file = None
+    while line := file_read.readline():
+        if search_word_in_line("\section", line):
+            file_write.write("= " + line.split("{")[1].split("}")[0] + "\n")
+            file_write.write(":stem: latexmath\n")
+            file_write.write(":xrefstyle: short\n")
+            line = ""
+        
+        if search_word_in_line("\subsection", line):
+            num_subsection += 1
+            subsection_file = "subsec_" + str(num_subsection) + ".adoc"
+            file_write = open(page_dir + name_section_file + "/" + subsection_file, 'w')
+            file_write.write("= " + line.split("{")[1].split("}")[0] + "\n")
+            file_write.write(":stem: latexmath\n")
+            file_write.write(":xrefstyle: short\n")
+            line=""
+        
+        if search_word_in_line("\graphicspath", line):
+            line = ":imagesdir: \{moduledir\}/assets/" + line.split("{")[2].split("}")[0] + "\n"
+
+        if search_word_in_line("\modif", line):
+            sentence = line.split("\modif")[1].split("{")[1].split("}")[0]
+            to_replace = "\modif{" + sentence + "}"
+            line = line.replace(to_replace, "#" + sentence + "#")
+
+        if search_word_in_line("$",line):
+            tab_line = line.split("$")
+            start_stem = 0
+            line_modif = ""
+            for i,part in enumerate(tab_line):
+                line_modif += part
+                if start_stem==0 and i!=len(tab_line)-1:
+                    line_modif += "stem:["
+                    start_stem = 1
+                elif start_stem==1 and i!=len(tab_line)-1:
+                    line_modif += "]"
+                    start_stem = 0
+            line = line_modif
+
+        if search_word_in_line("\\begin{figure}", line):
+            while not search_word_in_line("\end{figure}", line):
+                line = file_read.readline()
+                if search_word_in_line("\includegraphics", line):
+                    image_name = line.split("{")[1].split("}")[0]
+                    image_name = image_name[1:-1]   
+
+                    linewidth = line.split("width=")[1].split("\\linewidth")[0]
+                    if linewidth=="":
+                        linewidth="1.0"
+                    linewidth = float(linewidth)
+
+                    width = linewidth * 600
+                    height = linewidth * 480
+
+                if search_word_in_line("\caption", line):
+                    caption = line.split("{")[2].split("}")[0]
+
+                if search_word_in_line("\label", line):
+                    label = line.split("{")[1].split("}")[0]
+                    file_write.write("[["+label+"]]\n")
+
+            line = "."+caption+"\nimage::" + name_section_file + "/" + image_name + "[width="+str(width)+",height="+str(height)+"]\n"
+
+        if search_word_in_line("\\begin{equation", line):
+            line = "[stem]\n++++\n"
+        
+        if search_word_in_line("\end{equation", line):
+            line = "++++\n"
+
+        if search_word_in_line("\\begin{align", line):
+            line = "[stem]\n++++\n\\begin{aligned}\n"
+
+        if search_word_in_line("\end{align", line):
+            line = "\\end{aligned}\n++++\n"
+
+        if search_word_in_line("\subsubsection", line):
+            name_subsubsection = line.split("{")[1].split("}")[0]
+            line = "== " + name_subsubsection + "\n"
+
+        if search_word_in_line("\\begin{enumerate", line) or search_word_in_line("\end{enumerate", line):
+            line = "\n"
+        
+        if search_word_in_line("\item",line):
+            line = line.replace("\item", "* ")
+
+        if search_word_in_line("\\begin{Rem",line):
+            line = "\n[NOTE]\n====\n"
+
+        if search_word_in_line("\\end{Rem",line):
+            line = "====\n"
+
+        ref,test= test_fig(line)
+        if test:
+            name_label_fig = line.split("\\ref{")[1].split("}")[0]
+            line = line.replace(ref+"{"+name_label_fig+"}","<<"+name_label_fig+">>")
+
+        ref,test = test_section(line)
+        if test:
+            name_label_sec = line.split("\\ref{")[1].split("}")[0]
+            line = line.replace(ref+"{"+name_label_sec+"}","xref:"+label_sections[name_label_sec]+".adoc"+"[Section "+name_label_sec+"]")
+
+        if search_word_in_line("\\newpage",line):
+            line=""
+
+        if line!="":
+            if line[0]=="\t":
+                line = line[1:]
+            file_write.write(line)
+    
+def cp_all_sections(section_files,sections):
+    label_sections = get_label_sections(section_files)
+    for i,(section,subsections) in enumerate(sections.items()):        
+        if section_files[i]!="":
+            cp_section(section_files[i],label_sections)
+        else:
+            section_file_name = "section_" + str(i)
+            file_write = open(page_dir + section_file_name + ".adoc", 'w')
+            file_write.write("= " + section + "\n\n")
+            file_write.write("#A COMPLETER !#\n")
+            file_write.close()
+
+def rm_all():
+    shutil.rmtree(page_dir)
+    os.mkdir(page_dir)
+    shutil.rmtree(images_dir)
+    os.mkdir(images_dir)
+    os.remove(result_dir + "nav.adoc")
+
+# si il y a déjà un dossier antora, on le supprime et on copie le dossier antora_base
+# rm_all()
+section_files,sections = list_sections()
+create_nav_file(section_files,sections)
+create_nav(section_files,sections)
+create_main_page_file(section_files,sections)
+cp_images()
+cp_all_sections(section_files,sections)
